c28980b225ebc3110278f556a733e28b7fdcf2f6
Madhurita19 <madhuritamukherjee400@gmail.com>
2026-02-07 01:00:23 +0530

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -9,8 +9,10 @@ instance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token'); // or sessionStorage
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+  config.headers = config.headers || {};
+  config.headers.Authorization = `Bearer ${token}`;
+}
+
     return config;
   },
   (error) => Promise.reject(error)
